f0d2db64ae8c6a225b3d662ada05fc573847a02d
Egor <surisu16@gmail.com>
2021-04-29 12:06:56 +0300

    did last doc-strings

--- data_and_functions.py
+++ data_and_functions.py
@@ -10,6 +10,9 @@ spark = SparkSession \
 
 
 def read_data(file_name):
+    """
+    Reads csv file from input_data
+    """
     return spark.read.csv(f'input_data/{file_name}', sep=r'\t', header=True, inferSchema=True)
 
 
@@ -21,10 +24,16 @@ info_crew = read_data('title.crew.tsv')
 
 
 def write_to_csv(data, file):
+    """
+    Makes one dataframe from group of and saves to file
+    """
     return data.coalesce(1).write.mode('overwrite').save(f'output_data/{file}', format='csv')
 
 
 def find_top_movie():
+    """
+    Filters each movie and return the top 100
+    """
     return info_cinema \
         .join(info_ratings, 'tconst', 'inner') \
         .where((col('titleType') == 'movie') & (col('numVotes') >= 100000)) \
@@ -35,12 +44,21 @@ top_100_movie = find_top_movie()
 
 
 def explode_genre(column):
+    """
+    Explode column by one which contains many strings
+    """
     return explode(split(column, ','))
 
 
 def make_window(part, ord):
+    """
+    Window function implementation
+    """
     return row_number().over(Window.partitionBy(part).orderBy(col(ord).desc()))
 
 
 def join_table(first, second, first_predicate, second_predicate, how='inner'):
+    """
+    Join implementation
+    """
     return first.join(second, first_predicate == second_predicate, how)
