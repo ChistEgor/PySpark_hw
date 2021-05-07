0dc44e0b97ef7b04c98466b65512f44d9d4e4224
ChistEgor <61418907+ChistEgor@users.noreply.github.com>
2021-05-07 16:31:32 +0300

    Moved under __main__

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import tasks
-import functions
-
-info_names = functions.read_data('name.basics.tsv')
-info_cinema = functions.read_data('title.basics.tsv')
-info_ratings = functions.read_data('title.ratings.tsv')
-info_staff = functions.read_data('title.principals.tsv')
-info_crew = functions.read_data('title.crew.tsv')
-
-# For 1-5
-top_all_time = functions.find_top_movie(info_cinema, info_ratings)
+import tasks, functions
 
 if __name__ == '__main__':
+
+    info_names = functions.read_data('name.basics.tsv')
+    info_cinema = functions.read_data('title.basics.tsv')
+    info_ratings = functions.read_data('title.ratings.tsv')
+    info_staff = functions.read_data('title.principals.tsv')
+    info_crew = functions.read_data('title.crew.tsv')
+
+    # For 1-5
+    top_all_time = functions.find_top_movie(info_cinema, info_ratings)
+
     functions.write_to_csv(tasks.get_top_movie(top_all_time), '1_1')
     functions.write_to_csv(tasks.find_top_movie_last_10y(top_all_time), '1_2')
     functions.write_to_csv(tasks.find_top_movie_60s(top_all_time), '1_3')
